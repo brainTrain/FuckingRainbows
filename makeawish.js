69d58d372ffab434810931f93f1d6f8ecac4d5ad
Brain Train <abductee@gmail.com>
2011-10-12 17:27:39 -0700

    sdaf

--- makeawish.js
+++ makeawish.js
@@ -50,7 +50,7 @@ window.bookmarklet({
 
             $('.partytehdreamz').live('click', function() {
                 partyMagic = !partyMagic;
-                $(this).toggleClass('leave the party... :(');
+                $(this).html('leave the party... :(');
                 if(partyMagic = true) {
 
                     var pusher = new Pusher('0b75eb1a823194806706');    
