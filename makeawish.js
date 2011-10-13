2dfa981321bdae1f1ce77de7c7ea91e2333acf03
Brian Schroeder <abductee@gmail.com>
2011-10-13 08:24:20 -0700

    fsdf

--- makeawish.js
+++ makeawish.js
@@ -59,15 +59,6 @@ window.bookmarklet({
                     }
                     if( !dreamMagic && partyMagic ) {
 
-                        var pusher = new Pusher('0b75eb1a823194806706');    
-                        var channel = pusher.subscribe('blah');
-
-                        channel.bind('click', function(data) {  //listens for 'chirp' events on my channel 'alert'
-                         
-                            $(':contains(' + data + ')').toggleClass('catchadream');
-
-                        });
-
                         if( $(this).attr('class') != 'partytehdreamz') {
                             var magicSelect = $(this).html();
                             $.ajax( {
@@ -81,6 +72,16 @@ window.bookmarklet({
                         return true; 
                     }
                 });  
+                if( !dreamMagic && partyMagic ) {
+                    var pusher = new Pusher('0b75eb1a823194806706');    
+                    var channel = pusher.subscribe('blah');
+
+                    channel.bind('click', function(data) {  //listens for 'chirp' events on my channel 'alert'
+                     
+                        $(':contains(' + data + ')').toggleClass('catchadream');
+
+                        });
+                }
 
 
         });
