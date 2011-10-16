aa82d95fedf6962328f5f4b89f99e3a66de0a110
Brian Schroeder <abductee@gmail.com>
2011-10-16 08:52:41 -0700

    sfds

--- makeawish.js
+++ makeawish.js
@@ -55,7 +55,7 @@ window.bookmarklet({
                     }
                     if( !dreamMagic && partyMagic ) {
                         var pusher = new Pusher('0b75eb1a823194806706');    
-                        var channel = pusher.subscribe('blah');
+                        var channel = pusher.subscribe(rainbowChannel);
 
                         channel.bind('click', function(data) {  //listens for 'chirp' events on my channel 'alert'
                          
