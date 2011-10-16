124ab1daf1aaff55f31ee97e75fcda2f078ebe2b
Brian Schroeder <abductee@gmail.com>
2011-10-15 18:45:43 -0700

    fsdf

--- frainbows.js
+++ frainbows.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
     $('*').click( function() {
-        if(!$(this).hasClass('.title') || $(this).parent().hasClass('.title')) {     
+        if(!$(this).hasClass('title') || $(this).parent().hasClass('title') || !$(this).hasClass('suggest_link') || $(this).parent().hasClass('suggest_link') ) {     
             $(this).toggleClass('catchadream');
             return false;
         } else {
