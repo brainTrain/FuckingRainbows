0516a8b7c839a2c74ee6a7e237e640ed163517ce
Brian Schroeder <abductee@gmail.com>
2011-10-31 22:55:28 -0700

    fsdf

--- makeawish.js
+++ makeawish.js
@@ -68,7 +68,7 @@ window.bookmarklet({
                             console.log('data: ');
                             console.log(data);
 
-                            var noSlash = data.split('\/').join('ff');
+                            var noSlash = data.split('\\\\').join('ff');
                             console.log('noslash: ');
                             console.log(noSlash);
                             console.log('===================');
